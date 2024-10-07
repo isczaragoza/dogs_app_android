e4f04ad28c3beb3dbb2c107eb0be1ecefe4ce156
isczaragoza <iscaragoza@gmail.com>
2024-10-07 13:34:10 -0600

    Clean Code

--- app/src/main/java/com/isczaragoza/dogsapp/data/network/services/DogApiService.kt
+++ app/src/main/java/com/isczaragoza/dogsapp/data/network/services/DogApiService.kt
@@ -1,14 +1,11 @@
 package com.isczaragoza.dogsapp.data.network.services
 
 import com.isczaragoza.dogsapp.data.dtos.DogResponseDTO
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 
-/**Servicio para la implementación de consultas*/
+/**Servicio para la implementación de peticiones*/
 interface DogApiService {
     @GET("api/1151549092634943488")
     suspend fun requestListOfDogs(): Response<List<DogResponseDTO>>
-    @GET("api/1151549092634943488")
-    suspend fun requestListOfDogsCall(): Call<Unit>
 }
